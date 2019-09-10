fa5b563612bbb13302c6f89d63194d80fa82698a
Olivier LASBLEIS <lasbleis.olivier@yahoo.fr>
2019-09-10 11:19:20 +0200

    debut projet 10/09/2019 11h

---
+++ src/main/java/fr/diginamic/AirDataApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirDataApplication.class, args);
+	}
+
+}
