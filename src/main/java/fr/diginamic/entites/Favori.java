b097b751e805736f1bb16ce914ba0895a36390d3
Eloi Turpin <eloi.turpin@gmail.com>
2019-09-12 11:21:25 +0200

    faori

--- src/main/java/fr/diginamic/entites/Favori.java
+++ src/main/java/fr/diginamic/entites/Favori.java
@@ -1,11 +1,15 @@
 package fr.diginamic.entites;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
-import javax.persistence.*;
 
 @Entity
 @Data
@@ -13,33 +17,34 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class Favori {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @ManyToOne
-    Commune commune;
+	@ManyToOne
+	Commune commune;
 
-    @ManyToOne
-    @JoinColumn(name = "UTILISATEUR_ID")
-    private Utilisateur utilisateur;
+	@ManyToOne
+	@JoinColumn(name = "UTILISATEUR_ID")
+	private Utilisateur utilisateur;
 
-    private Boolean weatherDescription =false;
-    private Boolean weatherIcon=false;
-    private Boolean temperature=false;
-    private Boolean pressure=false;
-    private Boolean humidity=false;
-    private Boolean tempMin=false;
-    private Boolean tempMax=false;
-    private Boolean windSpeed=false;
-    private Boolean windDegrees=false;
+	private Boolean weatherDescription = false;
+	private Boolean weatherIcon = false;
+	private Boolean temperature = false;
+	private Boolean pressure = false;
+	private Boolean humidity = false;
+	private Boolean tempMin = false;
+	private Boolean tempMax = false;
+	private Boolean windSpeed = false;
+	private Boolean windDegrees = false;
 
-    private Boolean mesureSO2=false;
-    private Boolean mesurePM25=false;
-    private Boolean mesurePM10=false;
-    private Boolean mesureO3=false;
-    private Boolean mesureNO2=false;
-    private Boolean mesureCO=false;
+	private Boolean mesureSO2 = false;
+	private Boolean mesurePM25 = false;
+	private Boolean mesurePM10 = false;
+	private Boolean mesureO3 = false;
+	private Boolean mesureNO2 = false;
+	private Boolean mesureCO = false;
 
+	private Boolean population = false;
 
 }
