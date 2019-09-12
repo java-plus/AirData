d9eeddd5a1f5f6d26a567d0667660ada658ce3e2
Eloi Turpin <eloi.turpin@gmail.com>
2019-09-12 16:52:32 +0200

    mesures par commune

--- src/main/java/fr/diginamic/repository/CommuneRepository.java
+++ src/main/java/fr/diginamic/repository/CommuneRepository.java
@@ -1,13 +1,17 @@
 package fr.diginamic.repository;
 
 import fr.diginamic.entites.Commune;
+import fr.diginamic.entites.MesureMeteo;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CommuneRepository extends JpaRepository<Commune,Integer> {
+public interface CommuneRepository extends JpaRepository<Commune, Integer> {
 
-    public Optional<Commune> findByCodeCommune(String codeCommune);
+	public Optional<Commune> findByCodeCommune(String codeCommune);
+
+	public Optional<MesureMeteo> find
 }
